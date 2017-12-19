1f411e4db94c35bf7cd87a309e06b5408c394ea2
Daniel Wei <danielwii0326@gmail.com>
2017-12-19 18:13:56 +0800

    add storybook support

--- pages/login.js
+++ pages/login.js
@@ -1,61 +1,12 @@
 import React from 'react';
-import { Button, Checkbox, Form, Icon, Input } from 'antd';
+import { Form } from 'antd';
 import { connect } from 'react-redux';
 
-import Layout from '../layout';
+import Login from '../components/Login';
 import { withReduxSaga } from '../store';
 
-const FormItem = Form.Item;
+const LoginPage = ({}) => (
+  <Login />
+);
 
-class NormalLoginForm extends React.Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  }
-
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <Layout>
-
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <FormItem>
-            {getFieldDecorator('userName', {
-              rules: [{ required: true, message: 'Please input your username!' }],
-            })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />)}
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(<Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="Password"
-            />)}
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: true,
-            })(<Checkbox>Remember me</Checkbox>)}
-            <a className="login-form-forgot" href="">Forgot password</a>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              Log in
-            </Button>
-            Or <a href="">register now!</a>
-          </FormItem>
-        </Form>
-      </Layout>
-    );
-  }
-}
-
-const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
-
-const WrappedNormalLoginFormConnector = connect()(WrappedNormalLoginForm);
-
-export default withReduxSaga(WrappedNormalLoginFormConnector);
+export default withReduxSaga(connect()(LoginPage));
