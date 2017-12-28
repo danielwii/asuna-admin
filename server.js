1a865f691257aa8a985982bd5c824a949d2fe3b9
Daniel Wei <danielwii0326@gmail.com>
2017-12-28 08:48:08 +0800

    feat: add custem server.js to support proxy server

---
+++ server.js
@@ -0,0 +1,32 @@
+const { createServer }       = require('http');
+const { createProxyServer }  = require('http-proxy');
+const { parse }              = require('url');
+const next                   = require('next');
+const { createConfigLoader } = require('node-buffs');
+
+const dev          = process.env.NODE_ENV !== 'production';
+const app          = next({ dev });
+const handle       = app.getRequestHandler();
+const proxy        = createProxyServer({});
+const configurator = createConfigLoader();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl    = parse(req.url, true);
+    const { pathname } = parsedUrl;
+
+    if (pathname.startsWith('/api/')) {
+      proxy.web(req, res, { target: configurator.loadConfig('PROXY_API') }, (e) => {
+        console.error('PROXY_API ERROR!', e.stack);
+      });
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+    console.log(`> ENV: ${process.env.NODE_ENV}`);
+  });
+});
