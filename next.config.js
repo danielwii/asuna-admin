819132d95168306f898ec3010eea9b2f72fe259d
Daniel Wei <danielwii0326@gmail.com>
2018-01-16 12:20:15 +0800

    feat: add Jarvis webpack analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const Jarvis = require('webpack-jarvis');
+
 module.exports = {
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty',
     };
+
+    config.plugins = config.plugins || [];
+    config.plugins.push(new Jarvis({ port: 1337 }));
 
     return config;
   },
