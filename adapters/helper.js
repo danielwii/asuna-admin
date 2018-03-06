f4b3946949c22faf789705734de188083ebf4e53
Daniel Wei <danielwii0326@gmail.com>
2018-03-07 04:52:10 +0800

    update: columns helper

--- adapters/helper.js
+++ adapters/helper.js
@@ -38,15 +38,17 @@ export const columnHelper = {
  * 通用配置
  */
 export const commonColumns = {
-  any      : any => columnHelper.generate(any, any.toUpperCase()),
-  id       : columnHelper.generate('id', 'ID'),
-  name     : columnHelper.generate('name', '名称'),
-  title    : columnHelper.generate('title', '标题'),
-  name_en  : columnHelper.generate('name_en', '英文名称'),
-  email    : columnHelper.generate('email', 'Email'),
-  createdAt: columnHelper.generateCalendar('createdAt', '创建时间'),
-  updatedAt: columnHelper.generateCalendar('updatedAt', '更新时间'),
-  actions  : columnHelper.generateActions,
+  any       : any => columnHelper.generate(any, any.toUpperCase()),
+  id        : columnHelper.generate('id', 'ID'),
+  name      : columnHelper.generate('name', '名称'),
+  title     : columnHelper.generate('title', '标题'),
+  name_en   : columnHelper.generate('name_en', '英文名称'),
+  email     : columnHelper.generate('email', 'Email'),
+  createdAt : columnHelper.generateCalendar('createdAt', '创建时间'),
+  created_at: columnHelper.generateCalendar('created_at', '创建时间'),
+  updatedAt : columnHelper.generateCalendar('updatedAt', '更新时间'),
+  updated_at: columnHelper.generateCalendar('updated_at', '更新时间'),
+  actions   : columnHelper.generateActions,
 };
 
 export const defaultColumns = actions => [
