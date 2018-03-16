8252d6481ab43902525f0f329afc030fefd7931d
Daniel Wei <danielwii0326@gmail.com>
2018-03-17 00:06:42 +0800

    update: next-redux-saga to 2.0

--- store/index.js
+++ store/index.js
@@ -20,7 +20,7 @@ import localForage from 'localforage';
 
 import { notificationsReducer, notificationsSagas } from './notifications.redux';
 
-import { appReducer, appSagas, appEpics }            from './app.redux';
+import { appEpics, appReducer, appSagas }            from './app.redux';
 import { authReducer, authSagas }                    from './auth.redux';
 import { routerReducer, routerSagas }                from './router.redux';
 import { menuReducer, menuSagas }                    from './menu.redux';
@@ -151,10 +151,24 @@ export const configureStore = (state = initialState) => {
 
   persistStore(store, persistConfig);
 
-  store.sagaTask = sagaMiddleware.run(rootSaga);
+  /**
+   * next-redux-saga depends on `runSagaTask` and `sagaTask` being attached to the store.
+   *
+   *   `runSagaTask` is used to rerun the rootSaga on the client when in sync mode (default)
+   *   `sagaTask` is used to await the rootSaga task before sending results to the client
+   *
+   */
+
+  store.runSagaTask = () => {
+    store.sagaTask = sagaMiddleware.run(rootSaga);
+  };
+
+  // run the rootSaga initially
+  store.runSagaTask();
+
   return store;
 };
 
 export function withReduxSaga(BaseComponent) {
-  return withRedux(configureStore)(nextReduxSaga(BaseComponent));
+  return withRedux(configureStore, state => state)(nextReduxSaga({ async: true })(BaseComponent));
 }
