569567e1c5f08258763e0da700439496d2d5ffe0
Daniel Wei <danielwii0326@gmail.com>
2018-03-20 02:33:01 +0800

    fix: collect issue

--- bin/collect.sh
+++ bin/collect.sh
@@ -10,6 +10,6 @@ rm -f static/libs/*
 cp $BIN_PATH/../antd/dist/antd.css static/libs/antd.css
 cp $BIN_PATH/../draft-js/dist/Draft.css static/libs/draft.css
 cp $BIN_PATH/../braft-editor/dist/braft.css static/libs/braft.css
-cp $BIN_PATH/../vi1deo.js/dist/video-js.css static/libs/video-js.css
+cp $BIN_PATH/../video.js/dist/video-js.css static/libs/video-js.css
 
 exit 0
