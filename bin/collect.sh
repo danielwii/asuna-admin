451f5d629562cebfd6bcbeb0dbe18c41ab7ecddc
Daniel Wei <danielwii0326@gmail.com>
2018-03-19 23:06:42 +0800

    update: bin

--- bin/collect.sh
+++ bin/collect.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -ex
+set -e
+
+BIN_PATH="$(yarn bin)"
 
 mkdir -p static/libs
 rm -f static/libs/*
 
-cp node_modules/antd/dist/antd.css static/libs/antd.css
-cp node_modules/draft-js/dist/Draft.css static/libs/draft.css
-cp node_modules/braft-editor/dist/braft.css static/libs/braft.css
-cp node_modules/video.js/dist/video-js.css static/libs/video-js.css
+cp $BIN_PATH/../antd/dist/antd.css static/libs/antd.css
+cp $BIN_PATH/../draft-js/dist/Draft.css static/libs/draft.css
+cp $BIN_PATH/../braft-editor/dist/braft.css static/libs/braft.css
+cp $BIN_PATH/../video.js/dist/video-js.css static/libs/video-js.css
