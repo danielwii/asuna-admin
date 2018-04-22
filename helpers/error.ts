8e4c226c898a79c4f9bf00e76e08a5a1782ebf38
Daniel Wei <danielwii0326@gmail.com>
2018-04-23 00:28:52 +0800

    feat(FormModel): add exception handler

---
+++ helpers/error.ts
@@ -0,0 +1,47 @@
+import { AxiosResponse } from 'axios';
+
+import * as R from 'ramda';
+
+import { createLogger, lv } from '.';
+
+interface FormError {
+  [key: string]: {
+    errors: {
+      field: string;
+      message: string;
+    }[];
+  };
+}
+
+const logger = createLogger('helpers:errors', lv.warn);
+
+export function isErrorResponse(response: AxiosResponse) {
+  return response.data.name === 'Error' ||
+    /^ASUNA__.+/.test(response.data.code)
+}
+
+export function toFormErrors(response: AxiosResponse): FormError | string | null {
+  if (response === null) {
+    return null;
+  }
+  if (response.status === 400) {
+    const exception   = response.data as Asuna.Error.ValidationException;
+    const errorFields = R.map((error: Asuna.Error.Validate): FormError => ({
+      [error.property]: {
+        errors: [{
+          field  : error.property,
+          message: R.values(error.constraints).join('; '),
+        }],
+      },
+    }))(exception.errors);
+    logger.log('[toFormErrors] --->', { response, errorFields });
+    return R.mergeAll(errorFields);
+  } else if (response.status === 500) {
+    // FIXME may be a normal exception
+    const exception = response.data as Asuna.Error.AsunaException;
+    return exception.message;
+  } else {
+    console.warn('[toFormErrors]', { response });
+    return response.data;
+  }
+}
