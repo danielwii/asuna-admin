224792e9b9039c8d3f9c54b960c169e2e4ebf9db
Daniel Wei <danielwii0326@gmail.com>
2019-02-06 15:29:22 +0800

    feat: add logger-settings component

--- src/components/Debug.tsx
+++ src/components/Debug.tsx
@@ -1,16 +1,72 @@
 import React, { useState } from 'react';
-import * as util from 'util';
-
-import LoaderInline from '@jetbrains/ring-ui/components/loader-inline/loader-inline';
+import * as _ from 'lodash';
+import { Col, Grid, Row } from '@jetbrains/ring-ui/components/grid/grid';
+import Panel from '@jetbrains/ring-ui/components/panel/panel';
+import Button from '@jetbrains/ring-ui/components/button/button';
+import ButtonGroup, { Caption } from '@jetbrains/ring-ui/components/button-group/button-group';
 
 export function DebugSettings(props) {
-  const { modules, onChange } = props;
-  const state = useState({});
-  // const [modules] = state;
+  const { modules } = props;
+  const [, setState] = useState({});
+
+  const resetLogger = () => {
+    _.keys(modules).forEach(module => (modules[module] = 'warn'));
+    setState({});
+  };
+
+  const setLoggerLevel = (module, level) => {
+    modules[module] = level;
+    setState({});
+  };
+
   return (
     <div>
-      <LoaderInline />
-      <pre>{util.inspect({ props, state, modules })}</pre>
+      <Grid>
+        {_.map(modules, (level, module) => (
+          <Row key={module}>
+            <Col xs={2}>
+              <div>
+                {module}:{level}
+              </div>
+            </Col>
+            <Col>
+              <ButtonGroup>
+                <Button
+                  danger
+                  active={level === 'error'}
+                  onClick={() => setLoggerLevel(module, 'error')}
+                >
+                  {'Error'}
+                </Button>
+                <Button
+                  danger
+                  active={level === 'warn'}
+                  onClick={() => setLoggerLevel(module, 'warn')}
+                >
+                  {'Warn'}
+                </Button>
+                <Button active={level === 'info'} onClick={() => setLoggerLevel(module, 'info')}>
+                  {'Info'}
+                </Button>
+                <Button active={level === 'debug'} onClick={() => setLoggerLevel(module, 'debug')}>
+                  {'Debug'}
+                </Button>
+                <Button active={level === 'trace'} onClick={() => setLoggerLevel(module, 'trace')}>
+                  {'Trace'}
+                </Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+        ))}
+      </Grid>
+
+      <Panel>
+        <Button primary onClick={resetLogger}>
+          {'Reset Logger'}
+        </Button>
+        {/*<Button>{'Cancel'}</Button>*/}
+        {/*<Button onClick={resetLogger}>{'Reset'}</Button>*/}
+      </Panel>
     </div>
   );
 }
