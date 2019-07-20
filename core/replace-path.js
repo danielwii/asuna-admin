7d44e2a033366ce6663dba04c446e8566208bbee
Daniel Wei <danielwii0326@gmail.com>
2019-07-20 16:16:38 +0800

    fix: build issue

--- core/replace-path.js
+++ core/replace-path.js
@@ -2,7 +2,7 @@
 const nodePath = require('path');
 // const paths = require('tsconfig-paths');
 const tsconfig = require('tsconfig-extends');
-const Project = require('ts-morph').default;
+const { Project } = require('ts-morph');
 const _ = require('lodash');
 
 const opts = {
