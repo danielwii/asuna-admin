e5cdb762d7807d656c32252ea6b2e3012d67769d
Daniel Wei <danielwii0326@gmail.com>
2020-03-26 19:29:05 +0800

    refactor: components helper

--- core/src/helpers/components.tsx
+++ core/src/helpers/components.tsx
@@ -1,13 +1,14 @@
 /** @jsx jsx */
 import { useQuery } from '@apollo/react-hooks';
-import { AsunaDrawerButton, RoundWrapper, ThumbImage, WithVariable } from '@asuna-admin/components';
-import { valueToUrl } from '@asuna-admin/core';
+import { AsunaDrawerButton, WithVariable } from '@asuna-admin/components';
 import { AppContext } from '@asuna-admin/core/context';
 import { createLogger } from '@asuna-admin/logger';
 import { css, jsx } from '@emotion/core';
 import { createStyles, Theme } from '@material-ui/core';
+import { Avatar, Tooltip } from 'antd';
 import { gql } from 'apollo-boost';
 import { WithFuture } from 'asuna-components';
+import * as _ from 'lodash';
 import * as React from 'react';
 
 const logger = createLogger('components:helper');
@@ -28,6 +29,7 @@ export const ComponentsHelper = {
     getPortrait,
     modelName,
     getTitle,
+    getTooltip,
     getText,
     getExtra,
     future,
@@ -36,14 +38,15 @@ export const ComponentsHelper = {
     getPortrait?: (info: T) => React.ReactNode;
     modelName: string;
     getTitle?: (info) => T | React.ReactNode;
+    getTooltip?: (info) => T | React.ReactNode;
     getText: (info: T) => React.ReactNode;
     getExtra?: (info: T) => React.ReactNode;
     future: Promise<any>;
   }): React.ReactElement => (
     <WithFuture future={() => future}>
-      {result => (
+      {(result) => (
         <WithVariable variable={getModel(result)}>
-          {info => (
+          {(info) => (
             <div
               css={css`
                 display: flex;
@@ -55,32 +58,38 @@ export const ComponentsHelper = {
             >
               {getPortrait && (
                 <WithVariable variable={getPortrait(info)}>
-                  {portrait => (
+                  {(portrait) =>
+                    _.isString(portrait) ? (
+                      <Avatar style={{ margin: '1px' }} shape="square" size="large" src={portrait} />
+                    ) : (
+                      portrait
+                    ) /*
                     <RoundWrapper>
                       <ThumbImage
                         height={50}
                         width={50}
                         src={valueToUrl(portrait, { type: 'image', thumbnail: { height: 100, width: 100 } })}
                       />
-                    </RoundWrapper>
-                  )}
+                    </RoundWrapper>*/}
                 </WithVariable>
               )}
-              <div>
-                {getTitle && (
-                  <React.Fragment>
-                    <b>{getTitle(info)}</b>
-                    <br />
-                  </React.Fragment>
-                )}
-                <AsunaDrawerButton text={getText(info)} modelName={modelName} record={info as any} />
-                {getExtra && (
-                  <React.Fragment>
-                    <br />
-                    {getExtra(info)}
-                  </React.Fragment>
-                )}
-              </div>
+              <Tooltip title={getTooltip?.(info)}>
+                <>
+                  {getTitle && (
+                    <React.Fragment>
+                      <b>{getTitle(info)}</b>
+                      <br />
+                    </React.Fragment>
+                  )}
+                  <AsunaDrawerButton text={getText(info)} modelName={modelName} record={info as any} />
+                  {getExtra && (
+                    <React.Fragment>
+                      <br />
+                      {getExtra(info)}
+                    </React.Fragment>
+                  )}
+                </>
+              </Tooltip>
             </div>
           )}
         </WithVariable>
@@ -129,8 +138,8 @@ export const KVHelper = {
       .upsert('kv__pairs', {
         body: { collection: identifier.collection || 'system.server', key: identifier.key, value: body },
       })
-      .then(response => (cb ? cb(response.data) : response.data))
-      .catch(reason => logger.error(reason));
+      .then((response) => (cb ? cb(response.data) : response.data))
+      .catch((reason) => logger.error(reason));
   },
   clear: (identifier: { key: string; collection?: string }, cb?): Promise<any> => {
     logger.log('clear', { identifier });
@@ -138,14 +147,14 @@ export const KVHelper = {
       .upsert('kv__pairs', {
         body: { collection: identifier.collection || 'system.server', key: identifier.key, value: null },
       })
-      .then(response => (cb ? cb(response.data) : response.data))
-      .catch(reason => logger.error(reason));
+      .then((response) => (cb ? cb(response.data) : response.data))
+      .catch((reason) => logger.error(reason));
   },
   destroy: (identifier: { key: string; collection?: string }, cb?): Promise<any> => {
     logger.log('clear', { identifier });
     return AppContext.adapters.api
       .destroyKv({ collection: identifier.collection || 'system.server', key: identifier.key } as any)
-      .then(response => (cb ? cb(response.data) : response.data))
-      .catch(reason => logger.error(reason));
+      .then((response) => (cb ? cb(response.data) : response.data))
+      .catch((reason) => logger.error(reason));
   },
 };
