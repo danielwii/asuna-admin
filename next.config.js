cffced83c8c78de306f11676d0227fa24362b887
Daniel Wei <danielwii0326@gmail.com>
2021-01-01 20:05:02 +0800

    fix: build issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable @typescript-eslint/no-var-requires */
 const R = require('ramda');
+// https://github.com/vercel/next.js/blob/master/errors/css-npm.md
 const withCss = require('@zeit/next-css');
 const { withDefaultNextConfigs } = require('./asuna-with-default-next');
 
